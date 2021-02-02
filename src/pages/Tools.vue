3dec9069c23b58104bc73c15f7ed7da0def2d22b
Justijn Depover <justijndepover@gmail.com>
2021-02-02 17:18:43 +0100

    abstracted navigation

--- src/pages/Tools.vue
+++ src/pages/Tools.vue
@@ -1,35 +1,29 @@
 <template>
-    <div id="app" class="flex flex-col h-screen relative">
-        <navigation />
+    <div class="w-full max-w-2xl mx-auto px-4">
+        <h1 class="text-base font-semibold text-gray-800 tracking-wide mb-4">A list of useful tools</h1>
 
-        <div class="w-full max-w-2xl mx-auto px-4">
-            <h1 class="text-base font-semibold text-gray-800 tracking-wide mb-4">A list of useful tools</h1>
+        <panel-link :to="{ name: 'keylogger' }">
+            KeyLogger
+            <template #description>
+                <p>Check what the <pre class="inline bg-gray-100 text-xs font-medium p-1 rounded">Keycode</pre> or <pre class="inline bg-gray-100 text-xs font-medium p-1 rounded">Key</pre> is for a particular key press</p>
+            </template>
+        </panel-link>
 
-            <panel-link :to="{ name: 'keylogger' }">
-                KeyLogger
-                <template #description>
-                    <p>Check what the <pre class="inline bg-gray-100 text-xs font-medium p-1 rounded">Keycode</pre> or <pre class="inline bg-gray-100 text-xs font-medium p-1 rounded">Key</pre> is for a particular key press</p>
-                </template>
-            </panel-link>
-
-            <panel-link :to="{ name: 'timestamp' }">
-                Timestamp
-                <template #description>
-                    <p>A set of timestamp functions</p>
-                </template>
-            </panel-link>
-        </div>
+        <panel-link :to="{ name: 'timestamp' }">
+            Timestamp
+            <template #description>
+                <p>A set of timestamp functions</p>
+            </template>
+        </panel-link>
     </div>
 </template>
 
 <script>
-import Navigation from '../components/Navigation.vue';
 import PanelLink from '../components/PanelLink.vue';
 
 export default {
     name: 'Page',
     components: {
-        Navigation,
         PanelLink,
     },
 };
--- src/pages/tools/Keylogger.vue
+++ src/pages/tools/Keylogger.vue
@@ -1,65 +1,56 @@
 <template>
-    <div id="app" class="flex flex-col h-screen relative">
-        <navigation />
+    <div class="w-full max-w-2xl mx-auto px-4">
+        <router-link :to="{ name: 'tools' }" class="inline-block outline-none focus:outline-none focus:bg-gray-200 -ml-2 -mt-1 px-2 py-1 rounded-md mb-2">
+            <h1 class="text-base font-semibold text-gray-800 tracking-wide flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                </svg>
+                <span>Keylogger</span>
+            </h1>
+        </router-link>
 
-        <div class="w-full max-w-2xl mx-auto px-4">
-            <router-link :to="{ name: 'tools' }" class="inline-block outline-none focus:outline-none focus:bg-gray-200 -ml-2 -mt-1 px-2 py-1 rounded-md mb-2">
-                <h1 class="text-base font-semibold text-gray-800 tracking-wide flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 mr-2">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                    </svg>
-                    <span>Keylogger</span>
-                </h1>
-            </router-link>
+        <p class="text-sm text-gray-600 mb-4">Press a key to get more information</p>
 
-            <p class="text-sm text-gray-600 mb-4">Press a key to get more information</p>
-
-            <div class="rounded-lg border border-gray-300">
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">which</span>
-                    <span class="text-gray-600 text-sm">{{ which }}</span>
-                </div>
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">shiftKey</span>
-                    <span class="text-gray-600 text-sm">{{ shiftKey }}</span>
-                </div>
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">ctrlKey</span>
-                    <span class="text-gray-600 text-sm">{{ ctrlKey }}</span>
-                </div>
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">altKey</span>
-                    <span class="text-gray-600 text-sm">{{ altKey }}</span>
-                </div>
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">metaKey</span>
-                    <span class="text-gray-600 text-sm">{{ metaKey }}</span>
-                </div>
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">key</span>
-                    <span class="text-gray-600 text-sm">{{ key }}</span>
-                </div>
-                <div class="border-b border-gray-300 p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">keyCode</span>
-                    <span class="text-gray-600 text-sm">{{ keyCode }}</span>
-                </div>
-                <div class="p-4 flex items-center justify-between">
-                    <span class="text-gray-600 text-sm">code</span>
-                    <span class="text-gray-600 text-sm">{{ code }}</span>
-                </div>
+        <div class="rounded-lg border border-gray-300">
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">which</span>
+                <span class="text-gray-600 text-sm">{{ which }}</span>
+            </div>
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">shiftKey</span>
+                <span class="text-gray-600 text-sm">{{ shiftKey }}</span>
+            </div>
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">ctrlKey</span>
+                <span class="text-gray-600 text-sm">{{ ctrlKey }}</span>
+            </div>
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">altKey</span>
+                <span class="text-gray-600 text-sm">{{ altKey }}</span>
+            </div>
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">metaKey</span>
+                <span class="text-gray-600 text-sm">{{ metaKey }}</span>
+            </div>
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">key</span>
+                <span class="text-gray-600 text-sm">{{ key }}</span>
+            </div>
+            <div class="border-b border-gray-300 p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">keyCode</span>
+                <span class="text-gray-600 text-sm">{{ keyCode }}</span>
+            </div>
+            <div class="p-4 flex items-center justify-between">
+                <span class="text-gray-600 text-sm">code</span>
+                <span class="text-gray-600 text-sm">{{ code }}</span>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Navigation from '../../components/Navigation.vue';
-
 export default {
     name: 'Page',
-    components: {
-        Navigation,
-    },
     data() {
         return {
             which: '',
