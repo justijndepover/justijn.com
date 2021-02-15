a07285822a08ae2b9dd2b1b00d81ae26e555eeed
Justijn Depover <justijndepover@gmail.com>
2021-02-15 21:13:58 +0100

    fix with image generator

--- src/pages/tools/ImageGenerator.vue
+++ src/pages/tools/ImageGenerator.vue
@@ -4,6 +4,7 @@
             <form @submit.prevent="setCanvas">
                 <text-input label="Enter the width" placeholder="1024" v-model="width" :error="widthError" class="mb-4" />
                 <text-input label="Enter the height" placeholder="768" v-model="height" :error="heightError" class="mb-4" />
+                <text-input label="Enter the svg width" placeholder="256" v-model="svgWidth" class="mb-4" />
                 <text-input label="Enter the background color" placeholder="#eeeeee" v-model="backgroundColor" class="mb-4" />
                 <text-input label="Enter the svg color" placeholder="#ffffff" v-model="svgColor" class="mb-4" />
 
@@ -20,7 +21,7 @@
         </div>
 
         <div class="hidden">
-            <svg ref="svg" :width="computedWidth" viewBox="0 0 511 511" xmlns="http://www.w3.org/2000/svg">
+            <svg ref="svg" :width="svgWidth" :height="svgWidth" viewBox="0 0 511 511" xmlns="http://www.w3.org/2000/svg">
                 <path :fill="svgColor" d="m133.320312 373.828125c-34.152343 0-64.53125-21.867187-75.5625-54.421875l-.746093-2.453125c-2.601563-8.621094-3.691407-15.871094-3.691407-23.125v-145.453125l-51.753906 172.757812c-6.65625 25.410157 8.511719 51.753907 33.960938 58.773438l329.878906 88.34375c4.117188 1.066406 8.234375 1.578125 12.289062 1.578125 21.246094 0 40.660157-14.101563 46.101563-34.882813l19.21875-61.117187zm0 0"/>
                 <path :fill="svgColor" d="m191.988281 149.828125c23.53125 0 42.664063-19.136719 42.664063-42.667969s-19.132813-42.667968-42.664063-42.667968-42.667969 19.136718-42.667969 42.667968 19.136719 42.667969 42.667969 42.667969zm0 0"/>
                 <path :fill="svgColor" d="m458.652344.492188h-320c-29.394532 0-53.332032 23.9375-53.332032 53.335937v234.664063c0 29.398437 23.9375 53.335937 53.332032 53.335937h320c29.398437 0 53.335937-23.9375 53.335937-53.335937v-234.664063c0-29.398437-23.9375-53.335937-53.335937-53.335937zm-320 42.667968h320c5.890625 0 10.667968 4.777344 10.667968 10.667969v151.445313l-67.390624-78.636719c-7.148438-8.382813-17.496094-12.863281-28.609376-13.117188-11.050781.0625-21.417968 4.96875-28.5 13.460938l-79.234374 95.101562-25.8125-25.75c-14.589844-14.589843-38.335938-14.589843-52.90625 0l-58.878907 58.859375v-201.363281c0-5.890625 4.777344-10.667969 10.664063-10.667969zm0 0"/>
@@ -49,6 +50,7 @@ export default {
             widthError: '',
             height: 768,
             heightError: '',
+            svgWidth: 256,
             svgColor: '#ffffff',
             backgroundColor: '#eeeeee',
             showCanvas: false,
@@ -76,27 +78,24 @@ export default {
 
             this.$refs.canvas.width = this.width;
             this.$refs.canvas.height = this.height;
-            const ctx = this.$refs.canvas.getContext('2d');
-            ctx.fillStyle = this.backgroundColor;
-            ctx.fillRect(0, 0, this.$refs.canvas.width, this.$refs.canvas.height);
 
+            const ctx = this.$refs.canvas.getContext('2d');
+            ctx.clearRect(0, 0, this.width, this.height);
+
+            ctx.fillStyle = this.backgroundColor;
+            ctx.fillRect(0, 0, this.width, this.height);
+
+            ctx.fillStyle = this.svgColor;
             const xml = new XMLSerializer().serializeToString(this.$refs.svg);
             const img = document.createElement('img');
             img.src = `data:image/svg+xml;base64,${btoa(xml)}`;
 
-            ctx.drawImage(img, (this.width / 2 - this.computedWidth / 2), (this.height / 2 - this.computedWidth / 2));
+            img.onload = () => {
+                ctx.drawImage(img, (this.width / 2 - this.svgWidth / 2), (this.height / 2 - this.svgWidth / 2), this.svgWidth, this.svgWidth);
 
-            this.$refs.link.href = this.$refs.canvas.toDataURL('image/jpg');
-            this.$refs.link.download = `${this.width}x${this.height}.jpg`;
-        },
-    },
-    computed: {
-        computedWidth() {
-            if (parseInt(this.width, 10) < parseInt(this.height, 10)) {
-                return this.width / 4;
-            }
-
-            return this.height / 4;
+                this.$refs.link.href = this.$refs.canvas.toDataURL('image/jpg');
+                this.$refs.link.download = `${this.width}x${this.height}.jpg`;
+            };
         },
     },
 };
